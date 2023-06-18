f1a1028978aa0c9509972bfb9a352af35d800ea3
1koder <aderohunbi.a.a@gmail.com>
2023-06-18 13:11:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
  * main - 101-quote.c
- * description - Write a C program that prints exactly with proper grammar, but the outcome is a piece of art
+ * description - Write a C that is a C
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	write(2,
-			"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
-			59);
+		"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+		 59);
+
 	return (1);
 }
 
