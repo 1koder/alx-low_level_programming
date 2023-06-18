4e720655ef83922110c45d15bbd32e11802f6fa3
1koder <aderohunbi.a.a@gmail.com>
2023-06-18 13:00:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - 6-size.c
- * description - Write a C program that prints the size of various types on the computer
+ * description - Write a C program that prints the size of vario
  *
  * Return: Always 0 (Success)
  */
